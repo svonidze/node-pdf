33766896a8e48ef8cd5f2edd78bcaa84a7e35aec
svonidze <svonidze@mail.ru>
2023-04-09 01:16:36 +0200

    fix package problems

--- controllers/pdf.controller.js
+++ controllers/pdf.controller.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
 const readFile = require('util').promisify(fs.readFile);
 const hbs = require('hbs');
-const pdf = require('html-pdf'); 
+const pdf = require('puppeteer-html-pdf');
 
-exports.print = async (req, res) => {  
+exports.print = async (req, res) => {
     const invoiceItems = [
         { 'item': 'Website Design', 'amount': 50.50 },
         { 'item': 'Hosting (3 months)', 'amount': 80.50 },
@@ -17,18 +17,19 @@ exports.print = async (req, res) => {
         'total_amount': 141.50
     }
 
-    const content  = await readFile('views/invoice.hbs','utf8');  
+    const content = await readFile('views/invoice.hbs', 'utf8');
     const template = hbs.compile(content);
-    const html     = template({ invoiceItems, invoiceData });
+    const html = template({ invoiceItems, invoiceData });
 
+    res.end(html);
+    return;
     const options = {
         base: `${req.protocol}://${req.get('host')}`, // http://localhost:3000
         format: 'A4'
     }
 
-    pdf.create(html, options).toBuffer((err, buffer) => { 
-        if (err) return console.log(err);
-        res.attachment('invoice.pdf')
-        res.end(buffer);
-    });
+    const buffer = await pdf.create(html, options);
+
+    //res.attachment('invoice.pdf')
+    res.end(buffer);
 }
